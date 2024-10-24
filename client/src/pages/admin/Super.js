2fd513736798c5b760f11bd08c943831e88fbfa9
lawrencejr <oputalawrence@gmail.com>
2024-10-24 22:10:11 +0100

    all super admin tables designed

--- client/src/pages/admin/Super.js
+++ client/src/pages/admin/Super.js
@@ -16,6 +16,9 @@ import AdminDd from "../../components/AdminDd";
 import DelPost from "../../components/modals/DelPost";
 
 import { posts } from "../../data/posts";
+import { bloggers } from "../../data/bloggers";
+import { catgories } from "../../data/categories";
+import { didUKnw } from "../../data/didUKnw";
 
 const Super = () => {
   const navigate = useNavigate();
@@ -30,6 +33,70 @@ const Super = () => {
         <h1>Super admin</h1>
         <h3>You're welcome to the super dongeon</h3>
       </div>
+      <div className="accounts">
+        <h1>Manage accounts</h1>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>User</th>
+                <th>Total posts</th>
+                <th>Total views</th>
+                <th>Total likes</th>
+                <th>Total share</th>
+                <th>Actions</th>
+                <th>Approve/Block</th>
+              </tr>
+            </thead>
+            <tbody>
+              {bloggers.map((blogger, i) => {
+                return (
+                  <tr key={i}>
+                    <td className="post">
+                      <div
+                        className="img"
+                        style={{
+                          backgroundImage: `url(${blogger.img})`,
+                        }}
+                      ></div>
+                      <div className="content">
+                        <strong>{blogger.fullname}</strong>
+                        <br />
+                        <small>@{blogger.username}</small>
+                      </div>
+                    </td>
+                    <td>7 post(s)</td>
+                    <td>
+                      15k <FaEye />
+                    </td>
+                    <td>
+                      6.5k <FaThumbsUp />
+                    </td>
+                    <td>
+                      8k <FaShare />
+                    </td>
+                    <td>
+                      <div className="actn-btns">
+                        <button id="edit" style={{ color: "green" }}>
+                          <FaEdit />
+                        </button>
+                        <button id="del" style={{ color: "red" }}>
+                          <FaTrash />
+                        </button>
+                      </div>{" "}
+                    </td>
+                    <td>
+                      <button id="approve-btn">Approve</button>&nbsp;
+                      <button id="block-btn">Block</button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
       <div className="posts">
         <h1>All posts</h1>
         <div className="table-container">
@@ -113,10 +180,85 @@ const Super = () => {
 
       <div className="categories">
         <h1>Categories</h1>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>Category</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {catgories.map((categ, i) => {
+                return (
+                  <tr key={i}>
+                    <td className="post">
+                      <div
+                        className="img"
+                        style={{
+                          backgroundImage: `url(${categ.img})`,
+                        }}
+                      ></div>
+                      <div className="content">
+                        <strong>{categ.name}</strong>
+                        <br />
+                        <small>{categ.text}</small>
+                      </div>
+                    </td>
+
+                    <td>
+                      <div className="actn-btns">
+                        <button id="edit" style={{ color: "green" }}>
+                          <FaEdit />
+                        </button>
+                        <button id="del" style={{ color: "red" }}>
+                          <FaTrash />
+                        </button>
+                      </div>{" "}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
 
       <div className="duks">
         <h1>Did you know?</h1>
+        <div className="table-container">
+          <table>
+            <thead>
+              <tr>
+                <th>S/N</th>
+                <th>Text</th>
+
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {didUKnw.map((post, i) => {
+                return (
+                  <tr key={i}>
+                    <td>{i + 1}</td>
+                    <td className="post">{post.text}</td>
+
+                    <td>
+                      <div className="actn-btns">
+                        <button id="edit" style={{ color: "green" }}>
+                          <FaEdit />
+                        </button>
+                        <button id="del" style={{ color: "red" }}>
+                          <FaTrash />
+                        </button>
+                      </div>{" "}
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
       </div>
     </main>
   );
