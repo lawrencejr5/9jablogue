e63af91e1203aa7618fffeebdfdfda90264fd348
lawrencejr <oputalawrence@gmail.com>
2024-12-03 07:53:52 +0100

    cleaned up upload middleware

--- server/middlewares/upload.js
+++ server/middlewares/upload.js
@@ -1,5 +1,4 @@
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -14,38 +13,4 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
-module.exports = upload;
-
-// cloudinary.config({
-//   cloud_name: "dijtcsqey",
-//   api_key: "389553529394387",
-//   api_secret: "_Uh6N4WMcfSH7D1G6QaeXMHwUQE",
-// });
-
-// const cloudinaryStorage = multer.diskStorage({
-//   destination: (req, file, cb) => cb(null, ""),
-//   filename: async (req, file, cb) => {
-//     const originalName = file.originalname;
-//     const extension = path.extname(originalName);
-//     const sanitizedName = originalName.replace(/[\s\(\)\.com]/g, "_");
-
-//     const filename = Date.now() + "_" + sanitizedName + extension;
-
-//     try {
-//       const uploadResponse = await cloudinary.uploader.upload(file.buffer, {
-//         resource_type: file.mimetype.startsWith("image/") ? "image" : "auto", // Set resource type based on mimetype
-//         public_id: filename,
-//         folder: "your_folder_name",
-//       });
-
-//       cb(null, uploadResponse.public_id);
-//     } catch (error) {
-//       console.error("Error uploading to Cloudinary:", error);
-//       cb(error, null);
-//     }
-//   },
-// });
-
-// const upload = multer({ storage: cloudinaryStorage });
-
 module.exports = upload;
